049a209144055e7dfb50603e5320fd878ae58790
Coleen Huang <coleenhuang@gmail.com>
2021-04-08 21:21:33 -0700

    added routing

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
 import './components/_matches.polyfill.js';
+
+//get the app container
+let app = document.querySelector('[data-app]');
+
+//Determines the view UI
+let page = app.getAttribute('data-app');
+
+if (page === 'start') {
+    //render the start page
+}
+if (page === 'game') {
+    //render the game page
+}
