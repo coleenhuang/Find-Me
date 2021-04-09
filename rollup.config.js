707c17e4f89c2e592cdb16553225ad8944857d62
Coleen Huang <coleenhuang@gmail.com>
2021-04-08 18:12:19 -0700

    added files for game page

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import pkg from './package.json';
 // Configs
 var configs = {
 	name: 'BuildToolsCookbook',
-	files: ['main.js', 'detects.js', 'another-file.js'],
+	files: ['main.js', 'game.js'],
 	formats: ['iife', 'es', 'amd', 'cjs'],
 	default: 'iife',
 	pathIn: 'src/js',
