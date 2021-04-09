9d6b4432a4b17fc1ca50126db4d3b2dc65b190f8
Coleen Huang <coleenhuang@gmail.com>
2021-04-08 21:03:16 -0700

    added function to create grid

--- src/js/components/matrix.js
+++ src/js/components/matrix.js
@@ -3,10 +3,28 @@ const cols = 5;
 const words = ['CAR', 'MARR', 'SHARK']
 
 
-var matrix = [
+let matrix = [
     ['Y', 'W', 'S', 'T','D'],
     ['L', 'E', 'H', 'C','R'],
     ['V', 'C', 'A', 'R','R'],
     ['U', 'S', 'R', 'O','A'],
     ['P', 'V', 'K', 'C','M'],
 ];
+
+function createGrid() {
+    let grid = document.createElement("div");
+    grid.className = "grid"
+    for (let i = 0; i < matrix.length; i++) {
+        let column = document.createElement("div");
+        column.className = "grid-column";
+        let items = '';
+        for (let j = 0; j < matrix[i][j]; j++) {
+            items += `<div class="grid-item">
+                            ${matrix[i][j]}
+                    </div>`;
+        }
+        column.innerHTML = items;
+        grid.append(column)
+        
+    }
+}
