5985966d0c4a65ed21a78c5b6d8d6a72bf314025
Coleen Huang <coleenhuang@gmail.com>
2021-04-10 18:45:38 -0700

    added function for horizontal selection

--- src/js/main.js
+++ src/js/main.js
@@ -17,8 +17,10 @@ if (page === "game") {
     //render the game page
     game.render();
     const grid = document.querySelector('#grid');
-    
+    const gridItems = document.querySelectorAll('.grid-item')
     let mousePressed = false;
+    let prevCell = null;
+    let currentCell = null;
 
     grid.addEventListener('mousedown', e => {
         mousePressed = true
@@ -27,17 +29,37 @@ if (page === "game") {
     grid.addEventListener('mousemove', e => {
         let row = e.target.getAttribute('data-row')
         let col = e.target.getAttribute('data-col')
+        
         if(mousePressed) {
             e.target.classList.add('selected')
             console.log(row, col)
+            if(!prevCell) {
+                prevCell = {row, col}
+            }
+            let nextCell = {row, col};
+            if (nextCell) {
+                currentCell = nextCell
+            }
+
+            if (prevCell.row !== nextCell.row) {
+                resetSelection()
+            }
+
         }
     })
 
+    
+
     grid.addEventListener('mouseup', e => {
         mousePressed = false
-        const gridItems = document.querySelectorAll('.grid-item')
-        gridItems.forEach(el => el.classList.remove('selected'))
+        
       } )
+
+    function resetSelection() {
+        prevCell = null;
+        currentCell = null;
+        gridItems.forEach(el => el.classList.remove('selected'))
+    }
 
 
 }
